dfc6b2e4737aa131b5212d08e77303a8cf6fa18f
Pedro Schneider <pedrotrschneider@gmail.com>
2021-03-20 19:45:38 -0300

    🚧 Continue weapon system

--- scenes/sword/sword.gd
+++ scenes/sword/sword.gd
@@ -4,7 +4,7 @@ var _garbage;
 
 export(NodePath) onready var _animation_player = get_node(_animation_player) as AnimationPlayer;
 
-var anim_speed: float = 5.0;
+var anim_speed: float = 4.0;
 
 func _ready():
 	_garbage = GameEvents.connect("attack", self, "_on_attack");
@@ -12,5 +12,4 @@ func _ready():
 	_animation_player.playback_speed = anim_speed;
 
 func _on_attack() -> void:
-	_animation_player.play("AttackGo");
-	_animation_player.queue("AttackReturn");
+	_animation_player.play("AttackGo2");
