11cdb80397d3eef48c9027e608610f97b792c754
Pedro Schneider <pedrotrschneider@gmail.com>
2021-03-21 21:48:51 -0300

    🚧 Improve titan test mesh and animation

---
+++ scenes/test_titan_no_root/titan_ai.gd
@@ -0,0 +1,56 @@
+extends KinematicBody
+
+var _garbage;
+
+export(NodePath) onready var _hitbox = get_node(_hitbox) as Area
+var _nav_mesh: NavigationMesh = preload("res://resources/nav_meshes/sample_town/R2H8.tres") as NavigationMesh;
+
+var _nav: Navigation;
+
+const move_speed: int = 1000;
+const move_acceleration: float = 100.0;
+const gravity: float = 9.8;
+
+var path: PoolVector3Array;
+var path_index: int = 0;
+
+var velocity: Vector3 = Vector3.ZERO;
+
+const MAX_HIT_POINTS: int = 2;
+var hit_points: int = 0;
+
+
+func _ready():
+	_garbage = _hitbox.connect("hit", self, "_on_hit");
+	
+	_nav = Navigation.new();
+	_garbage = _nav.navmesh_add(_nav_mesh, Transform.IDENTITY);
+	
+	var target_pos: Position3D = self.get_tree().get_nodes_in_group("target")[0];
+	path = _get_path_to(target_pos.global_transform.origin);
+
+
+func _physics_process(delta):
+	if(path_index < path.size()):
+		var path_position: Vector3 = Vector3(path[path_index].x, self.global_transform.origin.y, path[path_index].z)
+		var move_dir: Vector3 = (path_position - self.global_transform.origin);
+		if(move_dir.length() < 0.1):
+			path_index += 1;
+		else:
+			self.look_at(path_position, Vector3.UP);
+			_garbage = self.move_and_slide(move_dir.normalized() * move_speed * delta);
+
+
+func _get_path_to(destination: Vector3) -> PoolVector3Array:
+	path_index = 0;
+	return _nav.get_simple_path(self.global_transform.origin, destination);
+
+
+func _on_hit() -> void:
+	print("I have been hit");
+	hit_points += 1;
+	
+	if(hit_points >= MAX_HIT_POINTS):
+		print("I have been killed");
+		GameEvents.emit_enemy_killed_signal(self);
+		self.queue_free();
