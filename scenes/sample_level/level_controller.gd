30f7f74a3030383bb4c5e1ffce12c7d66cf161b4
pedrotrschneider <pedrotrschneider@gmail.com>
2021-03-22 17:19:08 -0300

    🚧 Add procedural titan spawning on sample level

--- scenes/sample_level/level_controller.gd
+++ scenes/sample_level/level_controller.gd
@@ -15,22 +15,32 @@ func _ready() -> void:
 	for spawn_pos in _spawn_positions_container.get_children():
 		spawn_positions.append(spawn_pos.global_transform.origin);
 	
-	for target_pos in titan_target_positions:
+	for target_pos in _titan_targets_paths:
 		titan_target_positions.append(get_node(target_pos).global_transform.origin);
 
 
 func _on_SpawnTitan_timeout() -> void:
 	var spawn_pos: Vector3 = spawn_positions[randi() % spawn_positions.size()];
+	
+	var target_pos: Vector3;
+	if(titan_target_positions.size() > 1):
+		target_pos = titan_target_positions[randi() % titan_target_positions.size()];
+	else:
+		target_pos = titan_target_positions[0];
+	
+	print(target_pos);
 	var titan_instance = _titan_res.instance();
 	self.add_child(titan_instance);
 	titan_instance.nav_mesh = _navmesh;
 	titan_instance.global_transform.origin = spawn_pos;
+	titan_instance.target_pos = target_pos;
 	titan_instance.init_pathfinding();
 
 
 func _on_TargetArea_body_entered(body) -> void:
 	if(body.is_in_group("enemy")):
 		titans_in_target_area += 1;
+		print(titans_in_target_area);
 
 
 func _on_TargetArea_body_exited(body) -> void:
--- scenes/test_titan_no_root/titan_ai_new.gd
+++ scenes/test_titan_no_root/titan_ai_new.gd
@@ -2,14 +2,23 @@ extends KinematicBody
 
 var _garbage;
 
+enum TITAN_STATES {
+	FOLLOW,
+	ATTACK
+}
+
 export(NodePath) onready var _hitbox = get_node(_hitbox) as Area
 
 var nav_mesh: NavigationMesh;
 var _nav: Navigation;
 
+var state: int;
+
 const move_speed: int = 1000;
 const move_acceleration: float = 100.0;
 const gravity: float = 9.8;
+
+var target_pos: Vector3;
 
 var path: PoolVector3Array;
 var path_index: int = 0;
@@ -22,23 +31,20 @@ var hit_points: int = 0;
 
 func _ready():
 	_garbage = _hitbox.connect("hit", self, "_on_hit");
-	
-#	_nav = Navigation.new();
-#	_garbage = _nav.navmesh_add(_nav_mesh, Transform.IDENTITY);
-#
-#	var target_pos: Position3D = self.get_tree().get_nodes_in_group("target")[0];
-#	path = _get_path_to(target_pos.global_transform.origin);
 
 
 func _physics_process(delta):
-	if(path_index < path.size()):
-		var path_position: Vector3 = Vector3(path[path_index].x, self.global_transform.origin.y, path[path_index].z)
-		var move_dir: Vector3 = (path_position - self.global_transform.origin);
-		if(move_dir.length() < 0.1):
-			path_index += 1;
-		else:
-			smooth_look_at(path_position, delta);
-			_garbage = self.move_and_slide(move_dir.normalized() * move_speed * delta);
+	if(state == TITAN_STATES.FOLLOW):
+		if(path_index < path.size()):
+			var path_position: Vector3 = Vector3(path[path_index].x, self.global_transform.origin.y, path[path_index].z)
+			var move_dir: Vector3 = (path_position - self.global_transform.origin);
+			if(move_dir.length() < 0.1):
+				path_index += 1;
+			else:
+				smooth_look_at(path_position, delta);
+				_garbage = self.move_and_slide(move_dir.normalized() * move_speed * delta);
+	elif(state == TITAN_STATES.ATTACK):
+		pass
 
 
 func smooth_look_at(pos: Vector3, delta: float) -> void:
@@ -47,11 +53,12 @@ func smooth_look_at(pos: Vector3, delta: float) -> void:
 
 
 func init_pathfinding() -> void:
+	state = TITAN_STATES.FOLLOW;
+	
 	_nav = Navigation.new();
 	_garbage = _nav.navmesh_add(nav_mesh, Transform.IDENTITY);
 	
-	var target_pos: Position3D = self.get_tree().get_nodes_in_group("target")[0];
-	path = _get_path_to(target_pos.global_transform.origin);
+	path = _get_path_to(target_pos);
 
 
 func _get_path_to(destination: Vector3) -> PoolVector3Array:
