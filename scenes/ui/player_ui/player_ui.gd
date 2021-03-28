63e1095a17d594754406760d307299b9e91ac4a8
pedrotrschneider <pedrotrschneider@gmail.com>
2021-03-28 15:03:16 -0300

    ✨ Add ui that show which hooks are grappled

--- scenes/ui/player_ui/player_ui.gd
+++ scenes/ui/player_ui/player_ui.gd
@@ -1,6 +1,8 @@
-extends VBoxContainer
+extends CanvasLayer
 
 export(NodePath) onready var bullet_time_rect = get_node(bullet_time_rect) as ColorRect;
+export(NodePath) onready var hook_1_ui = get_node(hook_1_ui) as ColorRect;
+export(NodePath) onready var hook_2_ui = get_node(hook_2_ui) as ColorRect;
 
 onready var player: RigidBody = self.get_tree().get_nodes_in_group("player")[0];
 
@@ -8,5 +10,8 @@ func _ready():
 	pass
 
 
-func _process(delta):
+func _process(_delta):
 	bullet_time_rect.rect_scale.x = player.bullet_time_stamina / player.MAX_BULLET_TIME_STAMINA;
+	
+	hook_1_ui.visible = (!player.hook_1_grapple_position == null);
+	hook_2_ui.visible = (!player.hook_2_grapple_position == null);
